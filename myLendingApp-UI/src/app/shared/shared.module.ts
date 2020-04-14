de88762fc2e580048518b83e0384ef55467bfb8d
jd_manrique <mrroboto2514@gmail.com>
2020-04-15 02:24:29 +0800

    Nav item highlighter

--- myLendingApp-UI/src/app/shared/shared.module.ts
+++ myLendingApp-UI/src/app/shared/shared.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { NgbModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavHighlighterDirective } from './directives/nav-highlighter.directive';
 
 @NgModule({
   declarations: [
-    NavHighlighterDirective
   ],
   imports: [
     CommonModule,
